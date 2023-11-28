7e77005fd029c9b41acdef71cc620f3076956035
fawad <fawadtahir124@gmail.com>
2023-11-29 02:43:59 +0500

    Add Message model and integrate messages with that model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import  get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username + " " + self.timestamp
+
+    def last_10_messages(self):
+        return  Message.objects.orderby('timestamp').all()[:10]
